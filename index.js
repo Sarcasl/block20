45e2e4a7ef8dd29795fe2ea1a75da98a6f133a83
Sarcasl <venwes@gmail.com>
2023-05-18 16:58:20 -0700

    Update

--- index.js
+++ index.js
@@ -8,10 +8,31 @@ const users = [
     { name: "Adam", age: 47, occupation: "teacher" },
     { name: "Robert", age: 72, occupation: "driver" },
 ];
-
-function main() {
-
+const addName = (event) =>{
+const root = document.getElementById("root")
+const ptag = document.createElement("p")
+ptag.innerText = "click"
+root.appendChild (ptag)
 }
+
+
+function main (){
+const root = document.getElementById("root")
+const p=document.createElement("h1")
+h1.innerText = "FREELANCERS"
+const ul = document.createElement("ul")
+ul.innerText = `${users[0].name,age,occupation}`
+}
+
+
+const addName = (event) =>{
+    const root = document.getElementById("root")
+    const ptag = document.createElement("p")
+    ptag.innerText = "click"
+    root.appendChild (ptag)
+    }                 
+
+
 
 //call the main function
 main();
